ceda71625145ec9ec49a80ce57d6ada7e9163360
Matheus <matheusmendesreis@outlook.com>
2024-04-02 07:58:52 -0300

    chore: add taiwlind

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,8 +1,15 @@
+import '@testing-library/jest-dom';
 import { render } from "@testing-library/react";
 import App from "../src/App";
+
 
 describe("App", () => {
     it("should work as expected", () => {
         render(<App />);
+    });
+
+    it("should have a h1 element", () => {
+        const { container } = render(<App />);
+        expect(container.querySelector("h1")).toBeInTheDocument();
     });
 });
